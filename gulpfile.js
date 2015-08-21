7a24411d56830af6e9838b0e27d64dd00e4749f1
Jake Archibald <jaffathecake@gmail.com>
2015-08-21 12:44:36 +0100

    JS building

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,14 @@ var gulp = require('gulp');
 var plugins = require('gulp-load-plugins')();
 var runSequence = require('run-sequence');
 var del = require('del');
+var assign = require('lodash/object/assign');
+var browserify = require('browserify');
+var watchify = require('watchify');
+var babelify = require('babelify');
+var hbsfy = require('hbsfy');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var mergeStream = require('merge-stream');
 
 gulp.task('clean', function (done) {
   del(['dist/*', '!dist/node_modules', '!dist/.git'], done);
@@ -9,16 +17,71 @@ gulp.task('clean', function (done) {
 
 gulp.task('css', function () {
   return gulp.src('public/scss/*.scss')
+    .pipe(plugins.sass.sync().on('error', plugins.sass.logError))
     .pipe(plugins.sourcemaps.init())
     .pipe(plugins.sass({ outputStyle: 'compressed' }))
     .pipe(plugins.sourcemaps.write('./'))
     .pipe(gulp.dest('build/public/css/'));
 });
 
+function createBundle(src) {
+  var customOpts = {
+    entries: [src],
+    debug: true
+  };
+  var opts = assign({}, watchify.args, customOpts);
+  var b = watchify(browserify(opts));
+
+  b.transform(babelify.configure({
+    stage: 1
+  }));
+
+  b.transform(hbsfy);
+  b.on('log', plugins.util.log);
+  return b;
+}
+
+function bundle(b, outputPath) {
+  var splitPath = outputPath.split('/');
+  var outputFile = splitPath[splitPath.length - 1];
+  var outputDir = splitPath.slice(0, -1).join('/');
+
+  return b.bundle()
+    // log errors if they happen
+    .on('error', plugins.util.log.bind(plugins.util, 'Browserify Error'))
+    .pipe(source(outputFile))
+    // optional, remove if you don't need to buffer file contents
+    .pipe(buffer())
+    // optional, remove if you dont want sourcemaps
+    .pipe(plugins.sourcemaps.init({loadMaps: true})) // loads map from browserify file
+       // Add transformation tasks to the pipeline here.
+    .pipe(plugins.sourcemaps.write('./')) // writes .map file
+    .pipe(gulp.dest('build/' + outputDir));
+}
+
+var jsBundles = {
+  'public/js/page.js': createBundle('./public/js/page/index.js')
+};
+
+gulp.task('js', function () {
+  return mergeStream.apply(null,
+    Object.keys(jsBundles).map(function(key) {
+      return bundle(jsBundles[key], key);
+    })
+  );
+});
+
 gulp.task('watch', function () {
   gulp.watch(['public/scss/**/*.scss'], ['css']);
+
+  Object.keys(jsBundles).forEach(function(key) {
+    var b = jsBundles[key];
+    b.on('update', function() {
+      return bundle(b, key);
+    });
+  });
 });
 
 gulp.task('serve', function(callback) {
-  runSequence('clean', 'css', 'watch', callback);
+  runSequence('clean', ['css', 'js'], 'watch', callback);
 });
