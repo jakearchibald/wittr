31f4d687661b2eac4b0b22087a0492ccfb96fca7
Jake Archibald <jaffathecake@gmail.com>
2015-08-27 16:32:05 +0100

    Custom HTML response

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,6 +1,7 @@
 self.addEventListener('fetch', function(event) {
-	// TODO: respond to all requests with an html response
-	// containing an element with class="a-winner-is-me".
-	// Ensure the Content-Type of the response is "text/html"
-  console.log(event.request);
+  event.respondWith(
+    new Response('<strong class="a-winner-is-me">Hello!</strong>', {
+      headers: {'Content-Type': 'text/html'}
+    })
+  );
 });
