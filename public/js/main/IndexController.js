e9c25d09eb2b963c4c5ebc03dc13bf0cd3837a03
Jake Archibald <jaffathecake@gmail.com>
2015-08-28 08:18:08 +0100

    Handling new sw & updates

--- public/js/main/IndexController.js
+++ public/js/main/IndexController.js
@@ -36,8 +36,14 @@ IndexController.prototype._registerServiceWorker = function() {
     });
   });
 
-  // TODO: listen for the controlling service worker changing
-  // and reload the page
+  // Ensure refresh is only called once.
+  // This works around a bug in "force update on reload".
+  var refreshing;
+  navigator.serviceWorker.addEventListener('controllerchange', function() {
+    if (refreshing) return;
+    window.location.reload();
+    refreshing = true;
+  });
 };
 
 IndexController.prototype._trackInstalling = function(worker) {
@@ -56,7 +62,7 @@ IndexController.prototype._updateReady = function(worker) {
 
   toast.answer.then(function(answer) {
     if (answer != 'refresh') return;
-    // TODO: tell the service worker to skipWaiting
+    worker.postMessage({action: 'skipWaiting'});
   });
 };
 
