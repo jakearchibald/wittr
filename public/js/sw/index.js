b2f78a9f072ab0c82028e09efae367f759a12247
Jake Archibald <jaffathecake@gmail.com>
2015-09-01 07:16:17 +0100

    SW install & cache

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -14,6 +14,9 @@ self.addEventListener('install', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
-  // TODO: respond with an entry from the cache if there is one.
-  // If there isn't, fetch from the network.
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
 });
