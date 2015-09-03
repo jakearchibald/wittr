91c5fa2833ada2b6b35f17d0d0ae286938924d72
Jake Archibald <jaffathecake@gmail.com>
2015-09-03 17:53:04 +0100

    Including idb for later

--- public/js/main/IndexController.js
+++ public/js/main/IndexController.js
@@ -1,5 +1,6 @@
 import PostsView from './views/Posts';
 import ToastsView from './views/Toasts';
+import idb from 'idb';
 
 export default function IndexController(container) {
   this._container = container;
