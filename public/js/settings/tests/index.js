fa8a7c8518fb201596cae9a7367a39b09e2b1b58
Jake Archibald <jaffathecake@gmail.com>
2015-10-19 12:35:40 +0100

    Adding tests

--- public/js/settings/tests/index.js
+++ public/js/settings/tests/index.js
@@ -70,36 +70,44 @@ export default {
   },
   registered() {
     return remoteEval(function() {
-      return !!navigator.serviceWorker.controller;
-    }).then(controlled => {
-      if (controlled) return ["Service worker successfully registered!", '3.gif', true];
+      if (navigator.serviceWorker.controller) return ["Service worker successfully registered!", '3.gif', true];
       return ["Doesn't look like there's a service worker registered :(", 'nope.gif', false];
     });
   },
   'sw-waiting': function() {
     return remoteEval(function() {
       return navigator.serviceWorker.getRegistration('/').then(reg => {
-        if (!reg) return 'noreg';
-        if (!reg.waiting) return 'nowaiting';
-        return 'waiting';
+        if (!reg) return ["Doesn't look like there's a service worker registered at all!", 'sad.gif', false];
+        if (!reg.waiting) return ["There's no service worker waiting", 'nope.gif', false];
+        return ["Yey! There's a service worker waiting!", "4.gif", true];
       });
-    }).then(result => {
-      if (result == 'noreg') return ["Doesn't look like there's a service worker registered at all!", 'sad.gif', false];
-      if (result == 'nowaiting') return ["There's no service worker waiting", 'nope.gif', false];
-      return ["Yey! There's a service worker waiting!", "4.gif", true];
     });
   },
   'sw-active': function() {
     return remoteEval(function() {
       return navigator.serviceWorker.getRegistration('/').then(reg => {
-        if (!reg) return 'noreg';
-        if (reg.waiting) return 'waiting';
-        return 'nowaiting';
+        if (!reg) return ["Doesn't look like there's a service worker registered at all!", 'sad.gif', false];
+        if (reg.waiting) return ["There's still a service worker waiting", 'nope.gif', false];
+        return ["No service worker waiting! Yay!", "5.gif", true];
       });
-    }).then(result => {
-      if (result == 'noreg') return ["Doesn't look like there's a service worker registered at all!", 'sad.gif', false];
-      if (result == 'waiting') return ["There's still a service worker waiting", 'nope.gif', false];
-      return ["No service worker waiting! Yay!", "5.gif", true];
+    });
+  },
+  'html-response': function() {
+    return remoteEval(function() {
+      return fetch('/').then(response => {
+        const type = response.headers.get('content-type');
+
+        if (!type || (type.toLowerCase() != 'text/html' && !type.toLowerCase().startsWith('text/html'))) {
+          return ["The response doesn't have the 'Content-Type: text/html' header", 'nope.gif', false];
+        }
+
+        return response.text().then(text => new DOMParser().parseFromString(text, 'text/html')).then(doc => {
+          if (doc.body.querySelector('.a-winner-is-me')) {
+            return ["Custom HTML response found! Yay!", "6.gif", true];
+          }
+          return ["Can't find an element with class 'a-winner-is-me'", 'nope.gif', false];
+        });
+      });
     });
   }
 };
