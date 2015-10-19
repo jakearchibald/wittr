4106e6aefe8e2cd22a25bbb9508ba02ec87f08ff
Jake Archibald <jaffathecake@gmail.com>
2015-10-19 15:16:08 +0100

    jpg => gif

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,7 +1,7 @@
 self.addEventListener('fetch', function(event) {
-  // TODO: only respond to requests with a
-  // url ending in ".jpg"
-  event.respondWith(
-    fetch('/imgs/dr-evil.gif')
-  );
+  if (event.request.url.endsWith('.jpg')) {
+    event.respondWith(
+      fetch('/imgs/dr-evil.gif')
+    );
+  }
 });
