fa3720df7d819e6ddb14e5a13f32ed083df0c977
Jake Archibald <jaffathecake@gmail.com>
2015-10-24 12:53:15 +0100

    Stubbed out activate

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,5 +1,8 @@
 self.addEventListener('install', function(event) {
   event.waitUntil(
+    // TODO: change the site's theme, eg swap the vars in public/scss/_theme.scss
+    // Ensure at least $primary-color changes
+    // TODO: change cache name to 'wittr-static-v2'
     caches.open('wittr-static-v1').then(function(cache) {
       return cache.addAll([
         '/',
@@ -10,6 +13,12 @@ self.addEventListener('install', function(event) {
         'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff'
       ]);
     })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    // TODO: remove the old cache
   );
 });
 
