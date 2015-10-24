e1d1f1b8569ea1093ca5016a97ae8dc1127e983c
Jake Archibald <jaffathecake@gmail.com>
2015-10-24 17:31:07 +0100

    Stubbing out methods

--- public/js/main/IndexController.js
+++ public/js/main/IndexController.js
@@ -14,10 +14,30 @@ export default function IndexController(container) {
 IndexController.prototype._registerServiceWorker = function() {
   if (!navigator.serviceWorker) return;
 
-  navigator.serviceWorker.register('/sw.js').then(function() {
-    console.log('Registration worked!');
-  }).catch(function() {
-    console.log('Registration failed!');
+  var indexController = this;
+
+  navigator.serviceWorker.register('/sw.js').then(function(reg) {
+    // TODO: if there's no controller, this page wasn't loaded
+    // via a service worker, so they're looking at the latest version.
+    // In that case, exit early
+
+    // TODO: if there's an updated worker already waiting, call
+    // indexController._updateReady()
+
+    // TODO: if there's an updated worker installing, track its
+    // progress. If it becomes "installed", call
+    // indexController._updateReady()
+
+    // TODO: otherwise, listen for new installing workers arriving.
+    // If one arrives, track its progress.
+    // If it becomes "installed", call
+    // indexController._updateReady()
+  });
+};
+
+IndexController.prototype._updateReady = function() {
+  var toast = this._toastsView.show("New version available", {
+    buttons: ['whatever']
   });
 };
 
