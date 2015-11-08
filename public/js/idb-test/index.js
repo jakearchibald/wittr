56a0bb2f8b4ce063fcb8d9073c468e9bf081a5a7
Jake Archibald <jaffathecake@gmail.com>
2015-11-08 12:41:49 +0000

    People IDB task

--- public/js/idb-test/index.js
+++ public/js/idb-test/index.js
@@ -1,8 +1,17 @@
 import idb from 'idb';
 
-var dbPromise = idb.open('test-db', 1, function(upgradeDb) {
-  var keyValStore = upgradeDb.createObjectStore('keyval');
-  keyValStore.put("world", "hello");
+var dbPromise = idb.open('test-db', 3, function(upgradeDb) {
+  switch(upgradeDb.oldVersion) {
+    case 0:
+      var keyValStore = upgradeDb.createObjectStore('keyval');
+      keyValStore.put("world", "hello");
+    case 1:
+      upgradeDb.createObjectStore('people', { keyPath: 'name' });
+    case 2:
+      var peopleStore = upgradeDb.transaction.objectStore('people');
+      peopleStore.createIndex('animal', 'favoriteAnimal');
+  }
+  // TODO: create an index on 'people' named 'age', ordered by 'age'
 });
 
 // read "hello" in "keyval"
@@ -25,7 +34,57 @@ dbPromise.then(function(db) {
 });
 
 dbPromise.then(function(db) {
-  // TODO: in the keyval store, set
-  // "favoriteAnimal" to your favourite animal
-  // eg "cat" or "dog"
+  var tx = db.transaction('keyval', 'readwrite');
+  var keyValStore = tx.objectStore('keyval');
+  keyValStore.put('cat', 'favoriteAnimal');
+  return tx.complete;
+}).then(function() {
+  console.log('Added favoriteAnimal:cat to keyval');
 });
+
+// add people to "people"
+dbPromise.then(function(db) {
+  var tx = db.transaction('people', 'readwrite');
+  var peopleStore = tx.objectStore('people');
+
+  peopleStore.put({
+    name: 'Sam Munoz',
+    age: 25,
+    favoriteAnimal: 'dog'
+  });
+
+  peopleStore.put({
+    name: 'Susan Keller',
+    age: 34,
+    favoriteAnimal: 'cat'
+  });
+
+  peopleStore.put({
+    name: 'Lillie Wolfe',
+    age: 28,
+    favoriteAnimal: 'dog'
+  });
+
+  peopleStore.put({
+    name: 'Marc Stone',
+    age: 39,
+    favoriteAnimal: 'cat'
+  });
+
+  return tx.complete;
+}).then(function() {
+  console.log('People added');
+});
+
+// list all cat people
+dbPromise.then(function(db) {
+  var tx = db.transaction('people');
+  var peopleStore = tx.objectStore('people');
+  var animalIndex = peopleStore.index('animal');
+
+  return animalIndex.getAll('cat');
+}).then(function(people) {
+  console.log('Cat people:', people);
+});
+
+// TODO: console.log all people ordered by age
