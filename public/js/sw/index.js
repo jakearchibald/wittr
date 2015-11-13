4f2c24537247891e802cce8275250caf8cca13bd
Jake Archibald <jaffathecake@gmail.com>
2015-11-13 12:52:00 +0000

    TODOs for custom response task

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,3 +1,6 @@
 self.addEventListener('fetch', function(event) {
+	// TODO: respond to all requests with an html response
+	// containing an element with class="a-winner-is-me".
+	// Ensure the Content-Type of the response is "text/html"
   console.log(event.request);
 });
