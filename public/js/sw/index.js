dc9a6e778b6c0ce28cb606ef49f65197fbf1748a
aniansson <ebba@aniansson.se>
2017-12-08 16:47:23 +0100

    Add a custom message to the browser with the assigned HTML class

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -2,5 +2,9 @@ self.addEventListener('fetch', function(event) {
 	// TODO: respond to all requests with an html response
 	// containing an element with class="a-winner-is-me".
 	// Ensure the Content-Type of the response is "text/html"
-  console.log(event.request);
+  event.respondWith(
+    new Response('<b class="a-winner-is-me">Hello!</b>', {
+      headers: {'Content-Type': 'text/html'}
+    })
+  );
 });
