0a8f55650566bbf1c30c3524cc5e1a0fca739248
aniansson <ebba@aniansson.se>
2017-12-12 16:18:52 +0100

    TYPO -.-

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,8 +1,9 @@
 self.addEventListener('fetch', function(event) {
   // TODO: only respond to requests with a
   // url ending in ".jpg"
-if (event.request.url.endsWith('.jpg'));
-  event.respondWith(
-    fetch('/imgs/dr-evil.gif')
-  );
+  if (event.request.url.endsWith('.jpg')) {
+    event.respondWith(
+      fetch('/imgs/dr-evil.gif')
+    )
+  };
 });
