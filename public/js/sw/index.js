a2507eb479336cf5eeb770f75cffd7c50501c8f5
aniansson <ebba@aniansson.se>
2017-12-12 15:58:14 +0100

    Respond to requests ending in .jpg

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,6 +1,7 @@
 self.addEventListener('fetch', function(event) {
   // TODO: only respond to requests with a
   // url ending in ".jpg"
+if (event.request.url.endsWith('.jpg'));
   event.respondWith(
     fetch('/imgs/dr-evil.gif')
   );
