b8ae69f91e0d14ef24b6c9cdfc0f22fc3b57d002
aniansson <ebba@aniansson.se>
2017-12-12 18:10:25 +0100

    Open cache and add URLs

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,16 +1,17 @@
 self.addEventListener('install', function(event) {
-  var urlsToCache = [
-    '/',
-    'js/main.js',
-    'css/main.css',
-    'imgs/icon.png',
-    'https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW3W8TclTUvlFyQ.woff',
-    'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff'
-  ];
-
   event.waitUntil(
     // TODO: open a cache named 'wittr-static-v1'
     // Add cache the urls from urlsToCache
+    caches.open('wittr-statis-v1').then(function(cache) {
+      return cache.addAll([
+        '/',
+        'js/main.js',
+        'css/main.css',
+        'imgs/icon.png',
+        'https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW3W8TclTUvlFyQ.woff',
+        'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff'
+      ]);
+    })
   );
 });
 
