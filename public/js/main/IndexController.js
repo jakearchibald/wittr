d023c93e13d24ccfacc509074e50a4708b9f85fa
Kai Zhang <kaizen.agility@gmail.com>
2018-01-12 15:06:26 -0500

    Added service worker

--- public/js/main/IndexController.js
+++ public/js/main/IndexController.js
@@ -3,65 +3,72 @@ import ToastsView from './views/Toasts';
 import idb from 'idb';
 
 export default function IndexController(container) {
-  this._container = container;
-  this._postsView = new PostsView(this._container);
-  this._toastsView = new ToastsView(this._container);
-  this._lostConnectionToast = null;
-  this._openSocket();
-  this._registerServiceWorker();
+    this._container = container;
+    this._postsView = new PostsView(this._container);
+    this._toastsView = new ToastsView(this._container);
+    this._lostConnectionToast = null;
+    this._openSocket();
+    this._registerServiceWorker();
 }
 
 IndexController.prototype._registerServiceWorker = function() {
-  // TODO: register service worker
+    // TODO: register service worker
+    if (!navigator.serviceWorker) return;
+
+    navigator.serviceWorker.register('/sw.js').then(function() {
+        console.log('Yay!');
+    }).catch(function() {
+        console.log('Boo!');
+    });
 };
 
 // open a connection to the server for live updates
 IndexController.prototype._openSocket = function() {
-  var indexController = this;
-  var latestPostDate = this._postsView.getLatestPostDate();
+    var indexController = this;
+    var latestPostDate = this._postsView.getLatestPostDate();
 
-  // create a url pointing to /updates with the ws protocol
-  var socketUrl = new URL('/updates', window.location);
-  socketUrl.protocol = 'ws';
+    // create a url pointing to /updates with the ws protocol
+    var socketUrl = new URL('/updates', window.location);
+    socketUrl.protocol = 'ws';
 
-  if (latestPostDate) {
-    socketUrl.search = 'since=' + latestPostDate.valueOf();
-  }
-
-  // this is a little hack for the settings page's tests,
-  // it isn't needed for Wittr
-  socketUrl.search += '&' + location.search.slice(1);
-
-  var ws = new WebSocket(socketUrl.href);
-
-  // add listeners
-  ws.addEventListener('open', function() {
-    if (indexController._lostConnectionToast) {
-      indexController._lostConnectionToast.hide();
+    if (latestPostDate) {
+        socketUrl.search = 'since=' + latestPostDate.valueOf();
     }
-  });
 
-  ws.addEventListener('message', function(event) {
-    requestAnimationFrame(function() {
-      indexController._onSocketMessage(event.data);
+    // this is a little hack for the settings page's tests,
+    // it isn't needed for Wittr
+    socketUrl.search += '&' + location.search.slice(1);
+
+    var ws = new WebSocket(socketUrl.href);
+
+    // add listeners
+    ws.addEventListener('open', function() {
+        if (indexController._lostConnectionToast) {
+            indexController._lostConnectionToast.hide();
+        }
     });
-  });
 
-  ws.addEventListener('close', function() {
-    // tell the user
-    if (!indexController._lostConnectionToast) {
-      indexController._lostConnectionToast = indexController._toastsView.show("Unable to connect. Retrying…");
-    }
+    ws.addEventListener('message', function(event) {
+        requestAnimationFrame(function() {
+            indexController._onSocketMessage(event.data);
+        });
+    });
 
-    // try and reconnect in 5 seconds
-    setTimeout(function() {
-      indexController._openSocket();
-    }, 5000);
-  });
+    ws.addEventListener('close', function() {
+        // tell the user
+        if (!indexController._lostConnectionToast) {
+            indexController._lostConnectionToast = indexController._toastsView.show("Unable to connect. Retrying…");
+        }
+
+        // try and reconnect in 5 seconds
+        setTimeout(function() {
+            indexController._openSocket();
+        }, 5000);
+    });
 };
 
 // called when the web socket sends message data
 IndexController.prototype._onSocketMessage = function(data) {
-  var messages = JSON.parse(data);
-  this._postsView.addPosts(messages);
+    var messages = JSON.parse(data);
+    this._postsView.addPosts(messages);
 };
